a338f9d31ef1978810672f5678daef6b398e1b6c
dosanz pillay <72185271+code-admirer@users.noreply.github.com>
2020-10-01 13:05:57 +0530

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,27 @@
+import turtle
+
+a = turtle.bgcolor("white")
+
+b = turtle.pensize(2)
+
+c = turtle.speed(100)
+
+for i in range(8):
+
+    for colours in ["red","white","blue","green","violet"]:
+
+        #turtle.color(colours)   
+
+        turtle.circle(150)   
+
+        turtle.left(10)
+
+   
+
+   
+
+turtle.hideturtle()
+
+turtle.color("blue")
+
+turtle.pensize(3)
